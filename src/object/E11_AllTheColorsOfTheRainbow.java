2886eb717f739e7ddf3b0cf38fc344e112a7cfad
Serg <s.ostafiichuk@gmail.com>
2017-03-20 18:03:13 +0200

    E12

---
+++ src/object/E11_AllTheColorsOfTheRainbow.java
@@ -0,0 +1,17 @@
+package object;
+
+/**
+ * Created by Serg on 20.03.2017.
+ */
+
+public class E11_AllTheColorsOfTheRainbow {
+    int anIntegerRepresentingColors;
+    void changeTheHueOfTheColor(int newHue) {
+        anIntegerRepresentingColors = newHue;
+    }
+    public static void main(String[] args) {
+        E11_AllTheColorsOfTheRainbow all =
+                new E11_AllTheColorsOfTheRainbow();
+        all.changeTheHueOfTheColor(27);
+    }
+}
---
+++ src/object/E12_LeftToReader.java
@@ -0,0 +1,10 @@
+package object;
+
+/**
+ * Created by Serg on 20.03.2017.
+ */
+public class E12_LeftToReader {
+    public static void main(String args[]) {
+        System.out.println("Exercise left to reader");
+    }
+}
