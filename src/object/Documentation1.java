84f499b6f2a647e67b502dcdf56971b54f0827d6
Serg <s.ostafiichuk@gmail.com>
2017-03-20 23:40:04 +0200

    E16 added

---
+++ src/object/Documentation1.java
@@ -0,0 +1,12 @@
+package object;
+
+/**
+ * Created by Serg on 20.03.2017.
+ */
+/** A class comment */
+public class Documentation1 {
+    /** A field comment */
+    public int i;
+    /** A method comment */
+    public void f() {}
+}
--- src/object/E12_LeftToReader.java
+++ src/object/E12_LeftToReader.java
@@ -3,8 +3,12 @@ package object;
 /**
  * Created by Serg on 20.03.2017.
  */
+import java.util.*;
+
 public class E12_LeftToReader {
-    public static void main(String args[]) {
-        System.out.println("Exercise left to reader");
+
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
     }
 }
